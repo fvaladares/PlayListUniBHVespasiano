6b14ce4abb7cffe6bb7938786a5053e9d9654a51
Fabricio Valadares <fabricio.valadares@gmail.com>
2026-09-02 17:47:29 -0300

    Comments added at some classes.

--- src/main/java/org/example/infrastructure/persistencia/MemoriaPlaylistRepositoryImpl.java
+++ src/main/java/org/example/infrastructure/persistencia/MemoriaPlaylistRepositoryImpl.java
@@ -1,4 +1,8 @@
 package org.example.infrastructure.persistencia;
 
+
+/**
+ * Classe que usa o ArraList para armazenar a playlist
+ */
 public class MemoriaPlaylistRepositoryImpl {
 }
--- src/main/java/org/example/infrastructure/persistencia/PlaylistRepository.java
+++ src/main/java/org/example/infrastructure/persistencia/PlaylistRepository.java
@@ -3,6 +3,11 @@ package org.example.infrastructure.persistencia;
 
 /**
  * Interface de acesso aos dados
+ *
+ */
+
+/*
+    TODO(implemente os métodos responsáveis por inserir, listar e excluir músicas da playlist)
  */
 public interface PlaylistRepository {
 }
